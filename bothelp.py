a0fdff2ce9b61be528041086dfca092f370ba73c
Tristan Hanson-Suggitt <tristan@wataq.co>
2021-10-05 23:11:28 -0700

    less info

--- bothelp.py
+++ bothelp.py
@@ -1,10 +1,8 @@
 
-default = """`aq calc x`: reduce x and return one random equivalent
+default = """`aq calc x`: reduces x and returns one random equivalent
 
-`aq calcs x`: reduce x and return up to 30 random equivalents
+`aq calcs x`: reduces x and returns up to 30 random equivalents
 
-`aq lookup x`: x must be a whole number. return up to 30 equivalents for x
+`aq lookup x`: x must be a whole number. returns up to 30 equivalents for x
 
-`aq roll`: get 2 random symbols for divination or whatever. the 2 results represent a transition from the first phase to the second, similar to i-ching's changing lines, where the first result is more pertinent + detailed and the 2nd is contextual backdrop
-
-AQbot will record unique words, messages, and queries in any channel it is allowed to read. It can't read threads. It won't save any message or query with a mention."""
+`aq roll`: gets 2 random symbols for divination or whatever. The 2 results represent a transition from the first phase to the second, similar to the I-Ching's changing lines. The first result is more pertinent + detailed and the 2nd is contextual backdrop."""
